8acd98fcd560acc28ba000ea201b691dc91e5d76
Allan Odgaard <git@abetarda.com>
2013-01-13 08:04:36 +0100

    Optional title accepted by link_to_relative_file
    Not all links should be the name of the file they link to.

--- Support/app/helpers/application_helper.rb
+++ Support/app/helpers/application_helper.rb
@@ -7,11 +7,11 @@ module ApplicationHelper
     @git ||= Git.new
   end
   
-  def link_to_relative_file(git, file_path, line = nil)
+  def link_to_relative_file(git, file_path, line = nil, title = nil)
     if line 
-      link_to_textmate(git.root_relative_path_for(file_path), git.path_for(file_path), line)
+      link_to_textmate(title || git.root_relative_path_for(file_path), git.path_for(file_path), line)
     else
-      link_to_mate(git.root_relative_path_for(file_path), git.path_for(file_path))
+      link_to_mate(title || git.root_relative_path_for(file_path), git.path_for(file_path))
     end
   end
 end
