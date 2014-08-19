1946bdb7a7acb8793127a0f30e02503371dc721f
Kai Wood <k.wood@kwood.eu>
2014-08-19 10:00:58 +0200

    Add spec for new behaviour of annotation parser

--- Support/spec/lib/commands/annotate_spec.rb
+++ Support/spec/lib/commands/annotate_spec.rb
@@ -18,6 +18,26 @@ describe SCM::Git do
     it "should parse out the author, msg, and revision" do
       line = @lines.first
       line[:rev].should == "26e2d189"
+      line[:filepath].should be_nil
+      line[:author].should == "Tim Harper"
+      line[:date].should == Time.parse("2008-03-02 00:24:40 -0700")
+      line[:text].should == 'require LIB_ROOT + "/parsers.rb"'
+    end
+  end
+  
+  describe "when parsing a annotate with a RENAMED file" do
+    before(:each) do
+      @lines = @annotate.parse_annotation(File.read("#{FIXTURES_DIR}/annotate_renamed.txt"))
+    end
+    
+    it "should parse out all items" do
+      @lines.should have(428).entries
+    end
+    
+    it "should parse out the author, msg, and revision" do
+      line = @lines.first
+      line[:rev].should == "26e2d189"
+      line[:filepath].should == "Support/lib/git.rb"
       line[:author].should == "Tim Harper"
       line[:date].should == Time.parse("2008-03-02 00:24:40 -0700")
       line[:text].should == 'require LIB_ROOT + "/parsers.rb"'
