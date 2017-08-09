8dd97cde669ff785715f735b811db81d8f957e34
Stefan Daschek <stefan@die-antwort.eu>
2017-08-09 16:37:29 +0200

    Fix annotate_controller_spec
    This broke in “6bb8956 Follow moved files in Annotate/Blame history”.
    
    Note that even this fixed spec still fails until “c01e5db Support positive and negative timezone offset”.

--- Support/spec/controllers/annotate_controller_spec.rb
+++ Support/spec/controllers/annotate_controller_spec.rb
@@ -9,8 +9,8 @@ describe AnnotateController do
   describe "when annotating" do
     before(:all) do
       ENV["TM_SELECTION"] = "1"
-      Git.command_response["annotate", "file.rb"] = fixture_file("annotate.txt")
-      Git.command_response["log", "--date=default", "--format=medium", "file.rb"] = fixture_file("log_with_diffs.txt")
+      Git.command_response["blame", "file.rb"] = fixture_file("blame.txt")
+      Git.command_response["log", "--date=default", "--format=medium", "--follow", "--name-only", "file.rb"] = fixture_file("log_with_diffs.txt")
       @output = capture_output do 
         dispatch(:controller => "annotate", :file_path => "file.rb")
       end
